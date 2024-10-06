7ed3dd4502eb18dd7daeeaec4e80ef6ac65e9ba0
Andy Pak <im.andy.pak@gmail.com>
2024-10-05 20:44:39 -0400

    Implement google civic info api to get legislators names

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,30 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = File.read('secret.key').strip
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+
+    legislator_names = legislators.map(&:name)
+
+    legislator_names.join(", ")
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
 puts 'Event Manager Initialized.'
 
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
@@ -6,15 +32,7 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
-end
+  legislators = legislators_by_zipcode(zipcode)
 
-def clean_zipcode(zipcode)
-  if zipcode.nil?
-    zipcode = '00000'
-  elsif zipcode.length < 5
-    zipcode = zipcode.rjust(5,'0')
-  elsif zipcode.length > 5
-    zipcode = zipcode[0..4]
-  end
+  puts "#{name} #{zipcode} #{legislators}"
 end
